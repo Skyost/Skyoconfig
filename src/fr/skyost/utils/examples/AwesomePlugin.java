569e79db2cbb7370dfc3aa3f04c7c19641f2c5fe
Skyost <skyost@mail.com>
2014-05-23 19:17:31 +0200

    Added examples.

---
+++ src/fr/skyost/utils/examples/AwesomePlugin.java
@@ -0,0 +1,41 @@
+package fr.skyost.utils.examples;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AwesomePlugin extends JavaPlugin {
+	
+	private PluginConfig config; //Can be static too !
+	
+	@Override
+	public final void onEnable() {
+		final Logger logger = getLogger();
+		try {
+			config = new PluginConfig(new File(this.getDataFolder(), "config.yml"));
+			config.load();
+			logger.log(Level.INFO, "Voici le champ \"configurableString\" : \"" + config.configurableString + "\".");
+		}
+		catch(InvalidConfigurationException ex) {
+			ex.printStackTrace();
+			logger.log(Level.SEVERE, "Oooops ! Something went wrong while loading the configuration !");
+			Bukkit.getPluginManager().disablePlugin(this);
+		}
+	}
+	
+	@Override
+	public final void onDisable() {
+		try {
+			config.save();
+		}
+		catch(InvalidConfigurationException ex) {
+			ex.printStackTrace();
+			getLogger().log(Level.SEVERE, "Oooops ! Something went wrong while saving the configuration !");
+		}
+	}
+	
+}
---
+++ src/fr/skyost/utils/examples/PluginConfig.java
@@ -0,0 +1,34 @@
+package fr.skyost.utils.examples;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import fr.skyost.utils.Skyoconfig;
+
+public class PluginConfig extends Skyoconfig {
+	
+	@ConfigOptions(name = "configurable.string.1")
+	public String configurableString = "This String is configurable like others fields below ;)";
+	@ConfigOptions(name = "configurable.int.1")
+	public int configurableInt1 = 1;
+	@ConfigOptions(name = "configurable.int.2")
+	public int configurableInt2 = 0;
+	@ConfigOptions(name = "configurable.list.1")
+	public List<String> configurableList = Arrays.asList("YAML list...", "So awesome !");
+	@ConfigOptions(name = "configurable.map.1")
+	public HashMap<String, String> configurableMap = new HashMap<String, String>() {
+		private static final long serialVersionUID = 1L; {
+			put("key1", "value1");
+			put("key2", "value2");
+		}
+	};
+	
+	public String configurable_string_2 = "This one does not needs the annotation but the field's name looks so bad !";
+	
+	public PluginConfig(final File configFile) {
+		super(configFile, Arrays.asList("This is an header...", "...Which looks more than one line !"));
+	}
+	
+}
